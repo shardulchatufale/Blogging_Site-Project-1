e5c85f04034d5a4309b9d366791312154828d50e
shardulchatufale <shardulschatufale@gmail.com>
2022-07-15 00:11:20 +0530

    project -1

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose
+  .connect(
+    'mongodb+srv://sagarpatel14:6jM0LEgFZ8l7EID9@cluster0.nuwvybs.mongodb.net/ram7651-DB',
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log('MongoDb is connected'))
+  .catch((err) => console.log(err));
+
+app.use('/', route);
+
+app.all('/**', (req, res) => {
+  res.status(404).send({ status: false, message: 'Page Not Found!' });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express app running on port ' + (process.env.PORT || 3000));
+});
